fee3d96437d0aee2e2ec6ad0a7d2767cf4df2fa8
ANSCoder <nimjea@gmail.com>
2019-11-09 13:53:21 +0530

    Class setup for EditContact.

---
+++ Contacts-App/Edit Contact/Interactor/EditContactIntractor.swift
@@ -0,0 +1,13 @@
+//
+//  EditContactIntractor.swift
+//  Contacts-App
+//
+//  Created by Anand Nimje on 09/11/19.
+//  Copyright © 2019 Anand. All rights reserved.
+//
+
+import Foundation
+
+class EditContactIntractor{
+    
+}
---
+++ Contacts-App/Edit Contact/WireFrame/EditContactWireFrame.swift
@@ -0,0 +1,13 @@
+//
+//  EditContactWireFrame.swift
+//  Contacts-App
+//
+//  Created by Anand Nimje on 09/11/19.
+//  Copyright © 2019 Anand. All rights reserved.
+//
+
+import Foundation
+
+class EditContactWireFrame{
+    
+}
